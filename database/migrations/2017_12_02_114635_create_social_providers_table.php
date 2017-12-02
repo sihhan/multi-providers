7870240be54ae0955a99aeef9b3c4ea55e127f09
sih han wu <sword801128@gmail.com>
2017-12-02 20:08:01 +0800

    atter column user_id、provider_id、provider

--- database/migrations/2017_12_02_114635_create_social_providers_table.php
+++ database/migrations/2017_12_02_114635_create_social_providers_table.php
@@ -15,6 +15,9 @@ class CreateSocialProvidersTable extends Migration
     {
         Schema::create('social_providers', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned()->references('id')->on('users');
+            $table->string('provider_id');
+            $table->string('provider');
             $table->timestamps();
         });
     }
