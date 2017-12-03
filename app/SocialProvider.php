0c6776b70402ede1408f43a0c6f1fe453d54661d
sih han wu <sword801128@gmail.com>
2017-12-03 10:42:19 +0800

    設定批量賦值 fillable

--- app/SocialProvider.php
+++ app/SocialProvider.php
@@ -8,6 +8,11 @@ class SocialProvider extends Model
 {
     //
 
+    // 批量賦值(設定哪些欄位可以使用 fillable)
+    protected $fillable = [
+        'provider_id','provider'
+    ];
+
     // 與User建立關聯
     function user(){
         return $this->belongTo(User::class);
